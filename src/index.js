8cf707c2dbcfba6b04e74174998e9675fe167c72
Eli <perwin119@gmail.com>
2023-02-14 14:56:42 -0600

    contexto en index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MyContext } from './contexto';
+import { MyContextProvider } from './contexto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <h1>Hola</h1>
+    <MyContextProvider>
+        
+    </MyContextProvider>
 );
 
 
