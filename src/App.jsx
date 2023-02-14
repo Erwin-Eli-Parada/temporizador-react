cb650d0a57975c44e2226ddd6feafe4dfabab3f4
Eli <perwin119@gmail.com>
2023-02-14 15:34:37 -0600

    reloj

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Definidor } from "./definidor";
+import { Reloj } from "./reloj";
 
 export function App(props){
     return(
@@ -8,6 +9,7 @@ export function App(props){
             <div id="cuerpo">
                 <Definidor titulo="Tiempo de descanso"></Definidor>
                 <Definidor titulo="Tiempo de sesion"></Definidor>
+                <Reloj></Reloj>
             </div>
         </div>
     );
---
+++ src/reloj.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function Reloj(props){
+
+    return(
+        <div id="reloj">
+            <h2>Sesion</h2>
+            <span id="contador"></span>
+            <div id="botones">
+                <button id="play-pause">
+
+                </button>
+                <button id="restar">
+
+                </button>
+            </div>
+        </div>
+    );
+}
